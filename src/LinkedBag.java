b8ccad3a9bcbcc2e4d91d49f58bb2e85dd35e26b
Andrew Ferguson <you@example.com>
2018-09-14 13:46:45 -0400

    initial files

---
+++ src/LinkedBag.java
@@ -0,0 +1,107 @@
+/**
+   A class of bags whose entries are stored in a chain of linked nodes.
+	The bag is never full.
+*/
+
+public class LinkedBag<T> implements BagInterface<T>{
+
+
+
+	private class Node 
+	{
+		private T    data; // Entry in bag
+		private Node next; // Link to next node
+	
+		private Node(T dataPortion)
+		{
+			this(dataPortion, null);	
+		} // end constructor
+		
+		private Node(T dataPortion, Node nextNode)
+		{
+			data = dataPortion;
+			next = nextNode;	
+		} // end constructor
+	} // end Node
+
+	
+	private Node firstNode;       // Reference to first node
+	private int numberOfEntries;
+
+	// Default constructor
+	public LinkedBag() 
+	{
+		
+		
+	} // end default constructor
+	
+	@Override
+	public int getCurrentSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean add(T newEntry) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+ 	// Locates a given entry within this bag.
+	// Returns a reference to the node containing the entry, if located,
+	// or null otherwise.
+	private Node getReferenceTo(T anEntry)
+	{
+		Node currentNode; 
+		
+		for (currentNode = firstNode;currentNode != null; currentNode = currentNode.next)
+		{
+			if (anEntry==currentNode.data)
+				return currentNode;
+		} // end while
+		return currentNode;
+	} // end getReferenceTo
+
+	@Override
+	public boolean remove(T anEntry) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getFrequencyOf(T anEntry) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean contains(T anEntry) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
